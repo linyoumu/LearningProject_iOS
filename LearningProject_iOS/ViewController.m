ac164d73e9cb7bb5b2586590320decf63eb6971f
linyoumu <848516118@qq.com>
2023-04-07 11:26:54 +0800

    添加自行编译的FFmpeg版本 （版本号5.1.2）

--- LearningProject_iOS/ViewController.m
+++ LearningProject_iOS/ViewController.m
@@ -6,6 +6,8 @@
 //
 
 #import "ViewController.h"
+#import "avformat.h"
+#import "timestamp.h"
 
 @interface ViewController ()
 
@@ -16,6 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    av_log_set_level(AV_LOG_DEBUG);
+
+    
+    const char * version = av_version_info();
+    const char * config = avutil_configuration();
+    av_log(NULL, AV_LOG_INFO, "当前ffmpeg版本号：%s\n\n", version);
+    av_log(NULL, AV_LOG_INFO, "当前ffmpeg配置：%s", config);
+    
+    
+    
 }
 
 
